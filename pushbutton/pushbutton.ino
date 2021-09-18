41445fb57d3f8849edb370be2497db0cf98b342e
Washington Fernandes de Barros <w_as@yahoo.com>
2021-09-18 16:58:10 -0300

    alteração no arquivo pushbutton.ino

--- pushbutton/pushbutton.ino
+++ pushbutton/pushbutton.ino
@@ -1,6 +1,27 @@
 
 #define led 4
 #define btn 3
+
+int btn_clicado=0;
+int btn_liberado=0;
+
+void troca_led(){
+    if (digitalRead(led)==HIGH){
+        digitalWrite(led,LOW);
+      }else{
+          digitalWrite(led,HIGH);
+      } 
+ }
+
+void keyup(){
+ if((btn_clicado==1) and (btn_liberado==1)){
+    btn_clicado=0;
+    btn_liberado=0;
+    troca_led();
+  } 
+ }
+
+ 
 void setup()
 {
   pinMode(led, OUTPUT);
@@ -9,8 +30,11 @@ void setup()
 
 void loop()
 {
-  if(digitalRead(btn)==HIGH)
-    digitalWrite(led,HIGH);
+  if(digitalRead(btn)==HIGH){
+    btn_clicado=1;
+    btn_liberado=0;
+  }
   else
-    digitalWrite(led,LOW);
+    btn_liberado=1;
+  keyup();
 }
