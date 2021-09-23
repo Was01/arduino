d303de7e13fa6fdeba0dbf90bab27dcdcd1aca4f
Washington Fernandes de Barros <w_as@yahoo.com>
2021-09-23 19:49:54 -0300

    Inserindo projeto ledRGB01

---
+++ ledRGB01/ledRGB01.ino
@@ -0,0 +1,34 @@
+#define ledR 2
+#define ledG 3
+#define ledB 4
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledR,HIGH); 
+  digitalWrite(ledG,LOW); 
+  digitalWrite(ledB,LOW);
+  delay(3000);
+   digitalWrite(ledR,LOW); 
+  digitalWrite(ledG,HIGH); 
+  digitalWrite(ledB,LOW);
+  delay(3000);
+  digitalWrite(ledR,LOW); 
+  digitalWrite(ledG,LOW); 
+  digitalWrite(ledB,HIGH);
+  delay(3000);
+ 
+  
+  
+
+
+
+}
