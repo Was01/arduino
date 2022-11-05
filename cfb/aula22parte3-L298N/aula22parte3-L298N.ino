8ef34237da6a0cb6e86318edbd1edcf44d545110
Washington Fernandes de Barros <w_as@yahoo.com>
2022-11-05 17:47:17 -0300

    Nova atualização

--- cfb/aula22parte3-L298N/aula22parte3-L298N.ino
+++ cfb/aula22parte3-L298N/aula22parte3-L298N.ino
@@ -3,15 +3,16 @@
 #  define velm1 3
 # define  m1a 4
 # define m1b 5
+#define pot A0
 
- int max=255;
- int i=255;
-  int tmp=5000;
+ int leitura;
+  int tmp=10000;
 
 void setup(){
   pinMode(velm1,OUTPUT);
     pinMode(m1a,OUTPUT);
     pinMode(m1b,OUTPUT);
+    pinMode(pot,INPUT);
   
  
     analogWrite(velm1,0);
@@ -24,27 +25,11 @@ void setup(){
 
 void loop(){
 
-   
-      analogWrite(velm1,i);
+      leitura=map(analogRead(pot),0,1023,0,255);
+      analogWrite(velm1,leitura);
       digitalWrite(m1a,HIGH);
       digitalWrite(m1b,LOW);
-      delay(tmp);
-      analogWrite(velm1,i);
-      digitalWrite(m1a,LOW);
-      digitalWrite(m1b,LOW);
-      delay(tmp);
-      analogWrite(velm1,i-85);
-      digitalWrite(m1a,LOW);
-      digitalWrite(m1b,HIGH);
-      delay(tmp);
-      analogWrite(velm1,i);
-      digitalWrite(m1a,LOW);
-      digitalWrite(m1b,LOW);
-      delay(tmp);
       
-    
-
-  
   
    
 }
