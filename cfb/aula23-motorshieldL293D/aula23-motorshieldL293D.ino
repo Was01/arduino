d00822749f27bb5189d948ff6708a14c4fc1f467
Washington Fernandes de Barros <w_as@yahoo.com>
2022-11-12 20:08:41 -0300

    Nova atualização

---
+++ cfb/aula23-motorshieldL293D/aula23-motorshieldL293D.ino
@@ -0,0 +1,30 @@
+
+# include<AFMotor.h>
+
+
+AF_DCMotor motor1(1);
+
+void setup() {
+  motor1.setSpeed(0);
+
+}
+
+void loop() {
+  
+  motor1.setSpeed(255);
+  motor1.run(FORWARD); //sentido horário
+  delay(5000);
+
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  delay(2000);
+
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD); //sentido anti-horário
+  delay(5000);
+
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  delay(2000);
+
+}
