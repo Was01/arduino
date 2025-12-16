4add89fb0d4c00abcab835f1f481517495f16189
Was01 <80862278+Was01@users.noreply.github.com>
2025-12-16 12:50:04 +0000

    Update par_impar.ino

--- senai/par_impar/par_impar.ino
+++ senai/par_impar/par_impar.ino
@@ -1,38 +1,53 @@
-
-# define led_verde 9
-# define led_vermelho 8
+#define led_verde 9
+#define led_vermelho 8
 
 
 void setup()
 {
- 
   Serial.begin(9600);
-  Serial.println("Arduino Uno");
-  pinMode(led_verde,OUTPUT);
-  pinMode(led_vermelho,OUTPUT);
+  Serial.println("--- Verificador Par/Impar Arduino ---");
+  Serial.println("Aguardando numero...");
+  
+  pinMode(led_verde, OUTPUT);
+  pinMode(led_vermelho, OUTPUT);
+  
+  // Garante que ambos os LEDs estao desligados ao iniciar
+  digitalWrite(led_verde, LOW);
+  digitalWrite(led_vermelho, LOW);
 }
 
 void loop()
 {
+  // Verifica se ha dados disponiveis para leitura
   if (Serial.available() > 0)
   {
-   
-    int num;
-    while (Serial.available() > 0)
+    // Le o proximo numero inteiro (int) enviado pela serial.
+    // Isso resolve o problema de ASCII vs. Valor.
+    int num = Serial.parseInt(); 
+    
+    // IMPORTANTE: Limpa o buffer serial apos a leitura do numero.
+    // Isso evita que o Arduino leia dados "antigos" ou o caracter de nova linha (newline).
+    while(Serial.available()) {
+        Serial.read();
+    }
+
+    Serial.print("Numero recebido: ");
+    Serial.println(num);
+    
+    // --- Lógica de Par ou Ímpar ---
+    if (num % 2 == 0)
     {
-      num = Serial.read();
-      num=int(num);
-      if(num%2==0){
-        Serial.println("par");
-        digitalWrite(led_verde,HIGH);
-        digitalWrite(led_vermelho,LOW);
-        
-      }
-      else{
-        Serial.println("impar");
-        digitalWrite(led_vermelho,HIGH);
-        digitalWrite(led_verde,LOW);
-      }
+      Serial.println("PAR");
+      digitalWrite(led_verde, HIGH);  // Liga LED Verde (Par)
+      digitalWrite(led_vermelho, LOW);
+    }
+    else
+    {
+      Serial.println("IMPAR");
+      digitalWrite(led_vermelho, HIGH); // Liga LED Vermelho (Ímpar)
+      digitalWrite(led_verde, LOW);
     }
   }
 }
+
+
