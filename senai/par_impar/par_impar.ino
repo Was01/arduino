3cb15d32b4c83c904dc5b39cade7b34cc6ccd052
Was01 <80862278+Was01@users.noreply.github.com>
2025-12-16 12:24:32 +0000

    Update par_impar.ino

--- senai/par_impar/par_impar.ino
+++ senai/par_impar/par_impar.ino
@@ -1,10 +1,15 @@
 
+# define led_verde 9
+# define led_vermelho 8
+
 
 void setup()
 {
  
   Serial.begin(9600);
   Serial.println("Arduino Uno");
+  pinMode(led_verde,OUTPUT);
+  pinMode(led_vermelho,OUTPUT);
 }
 
 void loop()
@@ -19,9 +24,14 @@ void loop()
       num=int(num);
       if(num%2==0){
         Serial.println("par");
+        digitalWrite(led_verde,HIGH);
+        digitalWrite(led_vermelho,LOW);
+        
       }
       else{
-        Serial.println("ímpar");
+        Serial.println("impar");
+        digitalWrite(led_vermelho,HIGH);
+        digitalWrite(led_verde,LOW);
       }
     }
   }
