606afb65fbb2011987361bea60baf415fc967b9a
Was01 <80862278+Was01@users.noreply.github.com>
2025-12-20 20:24:05 -0300

    Update bracoRobotico.ino

--- senai/bracoRobotico/bracoRobotico.ino
+++ senai/bracoRobotico/bracoRobotico.ino
@@ -1,54 +1,43 @@
 #include <Servo.h>
 
-#define sry A3
-#define srx A2
-#define vrx A1
-#define vry A0
+// Definição dos pinos para facilitar a leitura
+#define PIN_SRY A3
+#define PIN_SRX A2
+#define PIN_VRX A1
+#define PIN_VRY A0
 
-Servo s1;
-Servo s2;
-Servo s3;
-Servo s4;
-
-int leitura1=90;
-int leitura2=90;
-int leitura3=90;
-int leitura4=90;
-
+Servo s1, s2, s3, s4;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(vrx,INPUT);
-  pinMode(vry,INPUT);
-  pinMode(srx,INPUT);
-  pinMode(sry,INPUT);
 
-   s1.attach(7);
-   s1.write(leitura1);
-   s2.attach(6);
-   s2.write(leitura2);
-   s3.attach(5);
-   s3.write(leitura3);
-   s4.attach(4);
-   s4.write(leitura4);
+  // Atribuição dos pinos (Apenas uma vez no setup)
+  s1.attach(7);
+  s2.attach(6);
+  s3.attach(5);
+  s4.attach(4);
 
-
+  // Posição inicial centralizada
+  s1.write(90);
+  s2.write(90);
+  s3.write(90);
+  s4.write(90);
 }
 
 void loop() {
-  leitura1=map(analogRead(vrx),0,1023,0,180);
-  leitura2=map(analogRead(vry),0,1023,0,180);
-  leitura3=map(analogRead(srx),0,1023,0,180);
-  leitura4=map(analogRead(sry),0,1023,0,180);
-  
-   s1.attach(6);
-   s1.write(leitura1);
-   s2.attach(7);
-   s2.write(leitura2);
-   s3.attach(5);
-   s3.write(leitura3); 
-   s4.attach(4);
-   s4.write(leitura4);
-  
+  // Leitura e mapeamento direto
+  // Mapeamos de 0-1023 (analog) para 0-180 (graus do servo)
+  int pos1 = map(analogRead(PIN_VRX), 0, 1023, 0, 180);
+  int pos2 = map(analogRead(PIN_VRY), 0, 1023, 0, 180);
+  int pos3 = map(analogRead(PIN_SRX), 0, 1023, 0, 180);
+  int pos4 = map(analogRead(PIN_SRY), 0, 1023, 0, 180);
 
-} 
+  // Envia o comando para os servos
+  s1.write(pos1);
+  s2.write(pos2);
+  s3.write(pos3);
+  s4.write(pos4);
+
+  // Pequeno delay para estabilidade mecânica e reduzir ruído
+  delay(15); 
+}
